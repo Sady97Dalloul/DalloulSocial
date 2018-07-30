31a55bb6e12f49b6cc0a30409f856869a556f0b8
Sady Dalloul <sadydalloul@Sadys-Mac.local>
2018-07-30 12:45:27 +0300

    Completed setting up Firebase integration

---
+++ DalloulSocial/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  DalloulSocial
+//
+//  Created by Sady Dalloul on 7/30/18.
+//  Copyright © 2018 Sady Dalloul. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
